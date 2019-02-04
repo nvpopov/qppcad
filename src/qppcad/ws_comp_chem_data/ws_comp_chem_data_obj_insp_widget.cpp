660f0f7c12e3739b16fd64f6248afaa243278825
nvpopov <brodiaga38@gmail.com>
2019-02-04 17:40:46 +0800

    fix: added qt5 charts sup

--- src/qppcad/ws_comp_chem_data/ws_comp_chem_data_obj_insp_widget.cpp
+++ src/qppcad/ws_comp_chem_data/ws_comp_chem_data_obj_insp_widget.cpp
@@ -4,15 +4,121 @@ using namespace qpp;
 using namespace qpp::cad;
 
 
+void ws_comp_chem_data_obj_insp_widget_t::bind_to_item(ws_item_t *_binding_item) {
+
+    if (_binding_item && _binding_item->get_type() ==
+            ws_comp_chem_data_t::get_type_static()) {
+        ws_comp_chem_data_t *dp = _binding_item->cast_as<ws_comp_chem_data_t>();
+        if (dp) {
+            // std::cout << "accepted ws_comp_chem_data_t in ::bind_to_item" << std::endl;
+            b_ccd = dp;
+        }
+        else {
+            //std::cout << "ALL IS WRONG" << std::endl;
+            b_ccd = nullptr;
+        }
+    }
+
+    ws_item_obj_insp_widget_t::bind_to_item(_binding_item);
+}
+
+void ws_comp_chem_data_obj_insp_widget_t::update_from_ws_item() {
+
+    ws_item_obj_insp_widget_t::update_from_ws_item();
+
+    if (b_ccd) {
+        // std::cout << "ALL IS GOOD" << std::endl;
+        gen_ccd_info_prog_name->setText(
+                    QString::fromStdString(
+                        qpp::ccdprog2str[b_ccd->m_ccd->m_comp_chem_program]
+                    )
+                );
+        gen_ccd_info_run_type->setText(
+                    QString::fromStdString(
+                        qpp::ccdrt2str[b_ccd->m_ccd->m_run_t]
+                    )
+                );
+        switch (b_ccd->m_ccd->m_run_t) {
+
+        case comp_chem_program_run_t::rt_unknown :
+            tabBar()->setTabEnabled(1, false);
+            tabBar()->setTabEnabled(2, false);
+            break;
+
+        case comp_chem_program_run_t::rt_energy :
+            tabBar()->setTabEnabled(1, false);
+            tabBar()->setTabEnabled(2, false);
+            break;
+
+        case comp_chem_program_run_t::rt_geo_opt :
+            tabBar()->setTabEnabled(1, true);
+            tabBar()->setTabEnabled(2, false);
+            update_geo_opt();
+            break;
+
+        default :
+            break;
+        }
+
+        //
+    }
+}
+
+void ws_comp_chem_data_obj_insp_widget_t::unbind_item() {
+    ws_item_obj_insp_widget_t::unbind_item();
+    b_ccd = nullptr;
+}
+
+void ws_comp_chem_data_obj_insp_widget_t::update_geo_opt() {
+    gb_go_conv_series->clear();
+
+    for (int i = 0 ; i < b_ccd->m_ccd->m_steps.size(); i++) {
+        gb_go_conv_series->append(
+                    i, b_ccd->m_ccd->m_steps[i].m_toten);
+    }
+    gb_go_conv_chart->removeSeries(gb_go_conv_series);
+    gb_go_conv_chart->addSeries(gb_go_conv_series);
+
+}
+
 ws_comp_chem_data_obj_insp_widget_t::ws_comp_chem_data_obj_insp_widget_t()
-  : ws_item_obj_insp_widget_t() {
+    : ws_item_obj_insp_widget_t() {
 
-  tab_geo_opt = define_tab(tr("Geometry opt."));
-  tab_vibs = define_tab(tr("Vibrational analysis"));
+    tab_geo_opt = define_tab(tr("Geometry opt."));
+    tab_vibs = define_tab(tr("Vibrational analysis"));
 
-  ws_item_is_visible->hide();
-  ws_item_is_visible_label->hide();
+    ws_item_is_visible->hide();
+    ws_item_is_visible_label->hide();
 
-  tab_general->tab_inner_widget_layout->addStretch(0);
-  //tg_form_layout->
+    gb_gen_ccd_info = new QGroupBox(tr("CCD info"));
+    gb_gen_ccd_info_lt = new QFormLayout;
+    gen_ccd_info_prog_name = new QLabel;
+    gen_ccd_info_run_type = new QLabel;
+
+    pre_init_group_box(gb_gen_ccd_info, gb_gen_ccd_info_lt);
+    gb_gen_ccd_info_lt->addRow(tr("Program :"), gen_ccd_info_prog_name);
+    gb_gen_ccd_info_lt->addRow(tr("Run type :"), gen_ccd_info_run_type);
+    post_init_group_box(gb_gen_ccd_info, gb_gen_ccd_info_lt);
+
+    tab_general->tab_inner_widget_layout->addWidget(gb_gen_ccd_info);
+    tab_general->tab_inner_widget_layout->addStretch(0);
+
+    gb_go_conv_graph = new QGroupBox(tr("Energy convergence"));
+    gb_go_conv_graph_lt = new QHBoxLayout;
+    gb_go_conv_graph->setLayout(gb_go_conv_graph_lt);
+
+    gb_go_conv_series = new QLineSeries();
+    gb_go_conv_chart = new QChart();
+    gb_go_conv_chart_view = new QChartView(gb_go_conv_chart);
+    gb_go_conv_graph_lt->addWidget(gb_go_conv_chart_view);
+    gb_go_conv_chart->legend()->hide();
+    gb_go_conv_chart->addSeries(gb_go_conv_series);
+    //gb_go_conv_chart->createDefaultAxes();
+    tab_geo_opt->tab_inner_widget_layout->addWidget(gb_go_conv_graph);
+    tab_geo_opt->tab_inner_widget_layout->addStretch(0);
+
+
+
+
+    //tg_form_layout->
 }
--- src/qppcad/ws_comp_chem_data/ws_comp_chem_data_obj_insp_widget.hpp
+++ src/qppcad/ws_comp_chem_data/ws_comp_chem_data_obj_insp_widget.hpp
@@ -2,22 +2,49 @@
 #define QPP_CAD_WS_COMP_CHEM_DATA_OBJ_INSP_H
 
 #include <qppcad/ws_item_obj_insp_widget.hpp>
+#include <qppcad/ws_comp_chem_data/ws_comp_chem_data_obj_insp_widget.hpp>
+#include <qppcad/ws_comp_chem_data/ws_comp_chem_data.hpp>
+#include <QtCharts/QChartView>
+#include <QtCharts/QLineSeries>
+
+QT_CHARTS_USE_NAMESPACE
 
 namespace qpp {
 
-  namespace cad {
+namespace cad {
 
-    class ws_comp_chem_data_obj_insp_widget_t : public ws_item_obj_insp_widget_t {
+class ws_comp_chem_data_obj_insp_widget_t : public ws_item_obj_insp_widget_t {
 
-        Q_OBJECT
+    Q_OBJECT
 
-      public:
-        ws_item_tab_widget_t *tab_geo_opt;
-        ws_item_tab_widget_t *tab_vibs;
-        ws_comp_chem_data_obj_insp_widget_t();
-    };
+public:
+    ws_comp_chem_data_t *b_ccd{nullptr};
+    ws_item_tab_widget_t *tab_geo_opt;
+    ws_item_tab_widget_t *tab_vibs;
 
-  }
+    QLabel *tab_info_program;
+    QGroupBox *gb_gen_ccd_info;
+    QFormLayout *gb_gen_ccd_info_lt;
+    QLabel *gen_ccd_info_prog_name;
+    QLabel *gen_ccd_info_run_type;
+
+    //start tab geometry optimization
+    QGroupBox *gb_go_conv_graph;
+    QHBoxLayout *gb_go_conv_graph_lt;
+    QLineSeries *gb_go_conv_series;
+    QChart *gb_go_conv_chart;
+    QChartView *gb_go_conv_chart_view;
+    //end tab geometry optimization
+
+    void bind_to_item(ws_item_t *_binding_item) override;
+    void update_from_ws_item() override;
+    void unbind_item() override;
+
+    void update_geo_opt();
+    ws_comp_chem_data_obj_insp_widget_t();
+};
+
+}
 
 }
 #endif
