2fb546085896cbd11b31b7b82de464622614e028
nvpopov <brodiaga38@gmail.com>
2019-02-15 16:41:02 +0800

    fix: added outline for inplace labels

--- src/qppcad/ws_atoms_list/ws_atoms_list_labels_subsys.cpp
+++ src/qppcad/ws_atoms_list/ws_atoms_list_labels_subsys.cpp
@@ -1,6 +1,7 @@
 #include <qppcad/ws_atoms_list/ws_atoms_list_labels_subsys.hpp>
 #include <qppcad/ws_atoms_list/ws_atoms_list.hpp>
 #include <qppcad/app_state.hpp>
+#include <QFontMetrics>
 
 using namespace qpp;
 using namespace qpp::cad;
@@ -92,36 +93,62 @@ void ws_atoms_list_labels_subsys_t::render_in_place_overlay(QPainter &painter) {
   int w = painter.device()->width();
   int h = painter.device()->height();
 
-  int sh = 50;
+  int sh = 70;
   int max_types = 10;
   int ntypes = std::min(p_owner->m_geom->n_atom_types(), max_types);
-  int w_h = 60;
+  int w_h = 80;
   int padding_h = 10;
   int sph_padding = 5;
   int w_w = sh * ntypes + sph_padding * (ntypes+1);
 
   QRect lrect(w/2 - w_w/2, h - w_h - padding_h, w_w, w_h);
   QPen rectpen(QPen(Qt::black, 3, Qt::SolidLine));
+  QPen rectpen2(QPen(Qt::black, 2, Qt::SolidLine));
   QPainterPath path;
   path.addRoundedRect(lrect,10, 10);
   painter.fillPath(path, Qt::white);
   painter.setPen(rectpen);
   painter.drawPath(path);
-  painter.setFont(QFont(astate->m_font_name, 20));
+
+  QFont text_font(astate->m_font_name, 31, QFont::Weight::ExtraBold);
+  QColor text_fill_color = QColor::fromRgbF(1,1,1);
+  QString text;
+
+  QColor fill_color = QColor::fromRgbF(0, 0, 1);
 
   for (int i = 0; i < ntypes; i++) {
+     painter.setPen(rectpen);
      QRect sph(w/2 - w_w/2 + sh*i + sph_padding*(i+1), h - w_h - padding_h + sph_padding, sh, sh);
 
      auto ap_idx = ptable::number_by_symbol(p_owner->m_geom->atom_of_type(i));
-     QColor fill_color = QColor::fromRgbF(0, 0, 1);
+
      if (ap_idx) {
          vector3<float> color = ptable::get_inst()->arecs[*ap_idx - 1].m_color_jmol;
          fill_color.setRgbF(color[0], color[1], color[2]);
-       }
+       } else fill_color = QColor::fromRgbF(0, 0, 1);
+
      painter.setBrush(fill_color);
      painter.drawEllipse(sph);
-     painter.drawText(sph, Qt::AlignCenter,
-                      QString::fromStdString(p_owner->m_geom->atom_of_type(i)));
+
+     text = QString::fromStdString(p_owner->m_geom->atom_of_type(i));
+//     painter.setFont(text_font);
+//     painter.drawText(sph, Qt::AlignCenter, text);
+
+//     painter.setPen(rectpen2);
+//     painter.setFont(text_font_small);
+//     painter.drawText(sph, Qt::AlignCenter, text);
+
+     painter.setBrush(text_fill_color);
+     painter.setPen(rectpen2);
+     QPainterPath text_path;
+     QFontMetrics fmetric(text_font);
+     painter.setRenderHint(QPainter::Antialiasing);
+     text_path.addText((sph.left() + sph.right()) / 2 - fmetric.width(text) / 2,
+                       sph.bottom() - fmetric.height() / 2 + padding_h,
+                       text_font,
+                       text);
+     painter.drawPath(text_path);
+
     }
 
 }
