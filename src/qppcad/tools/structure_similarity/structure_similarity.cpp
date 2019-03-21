8d4a1b597cb4b31ebc9641382d475b71f301a951
nvpopov <brodiaga38@gmail.com>
2019-03-21 21:03:16 +0800

    mirror

--- src/qppcad/tools/structure_similarity/structure_similarity.cpp
+++ src/qppcad/tools/structure_similarity/structure_similarity.cpp
@@ -21,8 +21,10 @@ void structure_similarity_tool_t::exec(ws_item_t *item) {
 structure_similarity_widget_t::structure_similarity_widget_t() : QDialog () {
 
   setWindowTitle(tr("Structure similarity"));
-  widget_layout = new QVBoxLayout;
-  setLayout(widget_layout);
+  widget_lt = new QVBoxLayout;
+  widget_top_lt = new QHBoxLayout;
+  setLayout(widget_top_lt);
+  widget_top_lt->addLayout(widget_lt);
 
   gb_select_actors = new QGroupBox(tr("Actors"));
   gb_select_actors_lt = new QFormLayout;
@@ -32,14 +34,27 @@ structure_similarity_widget_t::structure_similarity_widget_t() : QDialog () {
   cmb_it2 = new QComboBox;
   cmb_ws2 = new QComboBox;
 
+  cmb_method = new QComboBox;
+  cmb_method->addItem(tr("Compare-naive"));
+  cmb_method->addItem(tr("Compare-tws_tree"));
+
+  btn_compute = new QPushButton(tr("Compute"));
+  connect(btn_compute,
+          &QPushButton::clicked,
+          this,
+          &structure_similarity_widget_t::compute_button_clicked);
+
   gb_select_actors_lt->addRow(tr("Workspace №1"), cmb_ws1);
   gb_select_actors_lt->addRow(tr("Item №1"), cmb_it1);
   gb_select_actors_lt->addRow(tr("Workspace №2"), cmb_ws2);
   gb_select_actors_lt->addRow(tr("Item №2"), cmb_it2);
+  gb_select_actors_lt->addRow(tr("Method"), cmb_method);
+  gb_select_actors_lt->addRow(tr(""), btn_compute);
 
   gb_select_actors->setLayout(gb_select_actors_lt);
 
-  widget_layout->addWidget(gb_select_actors);
+  widget_lt->addWidget(gb_select_actors);
+  //widget_lt->addSpacing(1);
 
   connect(cmb_ws1,
           static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
@@ -53,6 +68,39 @@ structure_similarity_widget_t::structure_similarity_widget_t() : QDialog () {
 
   cmb_ws(cmb_ws1);
   cmb_ws(cmb_ws2);
+
+  gb_str_sim_output = new QGroupBox(tr("Results"));
+  gb_str_sim_output_lt = new QVBoxLayout;
+  gb_str_sim_output->setLayout(gb_str_sim_output_lt);
+
+  str_sim_table = new QTableWidget;
+  QStringList table_hdr;
+  table_hdr.push_back("Atom");
+  table_hdr.push_back("Type1");
+  table_hdr.push_back("Type2");
+  table_hdr.push_back("|D|");
+  table_hdr.push_back("Dx");
+  table_hdr.push_back("Dy");
+  table_hdr.push_back("Dz");
+
+  str_sim_table->setColumnCount(7);
+  str_sim_table->setHorizontalHeaderLabels(table_hdr);
+  str_sim_table->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+  str_sim_table->verticalHeader()->setVisible(false);
+
+  str_sim_table->setSelectionMode(QAbstractItemView::SelectionMode::NoSelection);
+  str_sim_table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+  str_sim_table->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  str_sim_table->setMinimumWidth(550);
+  str_sim_table->setMinimumHeight(350);
+
+  str_sim_output = new QTextEdit;
+  str_sim_output->setMaximumHeight(110);
+
+  gb_str_sim_output_lt->addWidget(str_sim_table);
+  gb_str_sim_output_lt->addWidget(str_sim_output);
+
+  widget_top_lt->addWidget(gb_str_sim_output);
 
 }
 
@@ -95,6 +143,153 @@ void structure_similarity_widget_t::cmb_it(QComboBox *_cmb, std::shared_ptr<work
       _cmb->setEnabled(false);
 
     }
+
+}
+
+void structure_similarity_widget_t::compute_structure_similarity(ws_item_t *g1, ws_item_t *g2) {
+
+  str_sim_output->clear();
+  str_sim_table->clearContents();
+  str_sim_table->setRowCount(0);
+
+  str_sim_output->insertPlainText("Computing structure similarity...\n");
+
+  if (!g1) {
+      str_sim_output->insertPlainText("Item №1 is invalid\n");
+      return;
+    }
+
+  if (!g2) {
+      str_sim_output->insertPlainText("Item №2 is invalid\n");
+      return;
+    }
+
+  str_sim_output->insertPlainText("Is Item №1 has type of geom_view_t ?");
+  auto gv1 = g1->cast_as<geom_view_t>();
+  if (gv1) str_sim_output->insertPlainText(" Yes\n");
+  else str_sim_output->insertPlainText(" No\n");
+
+  str_sim_output->insertPlainText("Is Item №2 has type of geom_view_t ?");
+  auto gv2 = g2->cast_as<geom_view_t>();
+  if (gv2) str_sim_output->insertPlainText(" Yes\n");
+  else str_sim_output->insertPlainText(" No\n");
+
+  size_t compare_method = cmb_method->currentIndex();
+
+  if (compare_method == 0) {
+      compute_structure_similarity_naive(gv1, gv2);
+    }
+
+  if (compare_method == 1) {
+      str_sim_output->insertPlainText("Compare method = unimplemented\n");
+    }
+
+}
+
+void structure_similarity_widget_t::compute_structure_similarity_naive(geom_view_t *g1,
+                                                                       geom_view_t *g2) {
+
+  //Checking dimensions
+  if (g1->m_geom->DIM != g2->m_geom->DIM) {
+      str_sim_output->insertPlainText(tr("Dim1(%1) != Dim2(%2). Aborting\n")
+                                      .arg(g1->m_geom->DIM)
+                                      .arg(g2->m_geom->DIM));
+      return;
+    }
+
+  //Checking nat()
+  if (g1->m_geom->nat() != g2->m_geom->nat()) {
+      str_sim_output->insertPlainText(tr("nat1(%1) != nat2(%2). Aborting\n")
+                                      .arg(g1->m_geom->nat())
+                                      .arg(g2->m_geom->nat()));
+      return;
+    }
+
+  //Checking n_types
+  if (g1->m_geom->n_types() != g2->m_geom->n_types()) {
+      str_sim_output->insertPlainText(tr("Warning! ntypes1(%1) != ntypes2(%2).\n")
+                                      .arg(g1->m_geom->n_types())
+                                      .arg(g2->m_geom->n_types()));
+    }
+
+  //All checks passed
+  str_sim_output->insertPlainText("\nAll checks passed!\n");
+
+  str_sim_table->setRowCount(g1->m_geom->nat());
+
+  for (size_t i = 0; i < g1->m_geom->nat(); i++) {
+
+      vector3<float> p1 = g1->m_geom->pos(i);
+      vector3<float> p2 = g2->m_geom->pos(i);
+      vector3<float> dp = p2 - p1;
+      float dp_l = dp.norm();
+
+      QTableWidgetItem *atom_id = new QTableWidgetItem(QString("%1").arg(i));
+      atom_id->setTextAlignment(Qt::AlignCenter);
+      str_sim_table->setItem(i, 0, atom_id);
+
+      bool types_are_different = g1->m_geom->type_table(i) != g2->m_geom->type_table(i);
+      QTableWidgetItem *atype_g1 =
+          new QTableWidgetItem(QString::fromStdString(g1->m_geom->atom_name(i)));
+      atype_g1->setTextAlignment(Qt::AlignCenter);
+      if (types_are_different) atype_g1->setBackground(Qt::red);
+      str_sim_table->setItem(i, 1, atype_g1);
+
+      QTableWidgetItem *atype_g2 =
+          new QTableWidgetItem(QString::fromStdString(g2->m_geom->atom_name(i)));
+      atype_g2->setTextAlignment(Qt::AlignCenter);
+      if (types_are_different) atype_g2->setBackground(Qt::red);
+      str_sim_table->setItem(i, 2, atype_g2);
+
+      QTableWidgetItem *dp_lr = new QTableWidgetItem(QString("%1").arg(dp_l));
+      dp_lr->setTextAlignment(Qt::AlignCenter);
+      if (dp_l > 0.001f) dp_lr->setBackgroundColor(Qt::red);
+      str_sim_table->setItem(i, 3, dp_lr);
+
+      QTableWidgetItem *dp_x = new QTableWidgetItem(QString("%1").arg(dp[0]));
+      dp_x->setTextAlignment(Qt::AlignCenter);
+      if (abs(dp[0]) > 0.001f) dp_x->setBackgroundColor(Qt::red);
+      str_sim_table->setItem(i, 4, dp_x);
+
+      QTableWidgetItem *dp_y = new QTableWidgetItem(QString("%1").arg(dp[1]));
+      dp_y->setTextAlignment(Qt::AlignCenter);
+      if (abs(dp[1]) > 0.001f) dp_y->setBackgroundColor(Qt::red);
+      str_sim_table->setItem(i, 5, dp_y);
+
+      QTableWidgetItem *dp_z = new QTableWidgetItem(QString("%1").arg(dp[2]));
+      dp_z->setTextAlignment(Qt::AlignCenter);
+      if (abs(dp[2]) > 0.001f) dp_z->setBackgroundColor(Qt::red);
+      str_sim_table->setItem(i, 6, dp_z);
+
+    }
+
+}
+
+void structure_similarity_widget_t::compute_button_clicked() {
+
+  app_state_t *astate = app_state_t::get_inst();
+
+  ws_item_t *it1{nullptr};
+  ws_item_t *it2{nullptr};
+
+  size_t ws_i1 = 0;
+  size_t ws_i2 = 0;
+  size_t it_i1 = 0;
+  size_t it_i2 = 0;
+
+  if (cmb_ws1->isEnabled() && cmb_it1->isEnabled()) {
+      ws_i1 = cmb_ws1->currentIndex();
+      it_i1 = cmb_it1->currentIndex();
+      it1 = astate->ws_manager->m_ws[ws_i1]->m_ws_items[it_i1].get();
+    }
+
+  if (cmb_ws2->isEnabled() && cmb_it2->isEnabled()) {
+      ws_i2 = cmb_ws2->currentIndex();
+      it_i2 = cmb_it2->currentIndex();
+      it2 = astate->ws_manager->m_ws[ws_i2]->m_ws_items[it_i2].get();
+    }
+
+  compute_structure_similarity(it1, it2);
 
 }
 
--- src/qppcad/tools/structure_similarity/structure_similarity.hpp
+++ src/qppcad/tools/structure_similarity/structure_similarity.hpp
@@ -2,6 +2,10 @@
 #define QPP_CAD_TOOL_STRUCTURE_SIMILARITY
 #include <qppcad/qppcad.hpp>
 #include <qppcad/ws_item/ws_item_behaviour_manager.hpp>
+#include <qppcad/ws_item/geom_view/geom_view.hpp>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QHeaderView>
 
 namespace qpp {
 
@@ -17,21 +21,31 @@ namespace qpp {
 
         QGroupBox *gb_select_actors;
         QFormLayout *gb_select_actors_lt;
-        QVBoxLayout *widget_layout;
+        QHBoxLayout *widget_top_lt;
+        QVBoxLayout *widget_lt;
 
         QComboBox *cmb_ws1;
         QComboBox *cmb_it1;
         QComboBox *cmb_ws2;
         QComboBox *cmb_it2;
+        QComboBox *cmb_method;
+        QPushButton *btn_compute;
 
         QGroupBox *gb_str_sim_output;
         QVBoxLayout *gb_str_sim_output_lt;
+        QTextEdit *str_sim_output;
+        QTableWidget *str_sim_table;
+
         structure_similarity_widget_t();
         void cmb_ws(QComboBox *_cmb);
         void cmb_it(QComboBox *_cmb, std::shared_ptr<workspace_t> _ws);
+        void compute_structure_similarity(ws_item_t *g1, ws_item_t *g2);
+        void compute_structure_similarity_naive(geom_view_t *g1,
+                                                geom_view_t *g2);
 
       public slots:
 
+        void compute_button_clicked();
         void cmb_ws1_changed(int index);
         void cmb_ws2_changed(int index);
 
