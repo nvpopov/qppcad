7e04bb72853c73b4854de5b6beda549d2046d5db
nvpopov <brodiaga38@gmail.com>
2019-06-13 19:35:21 +0800

    mirror

--- src/qppcad/ws_item/geom_view/geom_view_selector_widget.cpp
+++ src/qppcad/ws_item/geom_view/geom_view_selector_widget.cpp
@@ -1,5 +1,5 @@
 #include <qppcad/ws_item/geom_view/geom_view_selector_widget.hpp>
-#include <qppcad/ws_item/geom_view/geom_view.hpp>
+#include <qppcad/ws_item/geom_view/geom_view_anim_subsys.hpp>
 #include <qppcad/app_state.hpp>
 
 using namespace qpp;
@@ -28,7 +28,7 @@ void qgeom_view_selector_widget_t::generate_list_gv_items() {
 
             list_item->setText(QString::fromStdString(list_item_name));
 
-            //m_sub_gvs.push_back(as_gv);
+            m_sub_gvs.push_back(as_gv);
 
           }
 
@@ -39,12 +39,13 @@ qgeom_view_selector_widget_t::qgeom_view_selector_widget_t(QWidget *parent) : QW
   app_state_t *astate = app_state_t::get_inst();
 
   setMinimumHeight(250);
-  setMinimumWidth(400);
+  setMinimumWidth(450);
 
   main_lt = new QHBoxLayout;
   setLayout(main_lt);
 
   list_gv = new QListWidget;
+  list_gv->setMinimumWidth(400);
 
   add_btn = new QPushButton(">>");
   add_btn_lt = new QVBoxLayout;
@@ -52,12 +53,85 @@ qgeom_view_selector_widget_t::qgeom_view_selector_widget_t(QWidget *parent) : QW
   add_btn_lt->addWidget(add_btn);
   add_btn_lt->addSpacing(1);
 
-  list_gv_res = new QListWidget;
+  connect(add_btn,
+          &QPushButton::clicked,
+          this,
+          &qgeom_view_selector_widget_t::add_btn_clicked);
+
+  list_gv_res = new qgeom_view_result_widget_t;
+  list_gv_res->setMinimumWidth(400);
+
+  list_gv_res_scroll_area = new QScrollArea;
+  list_gv_res_scroll_area->setMinimumWidth(450);
+  list_gv_res_scroll_area->setWidget(list_gv_res);
+  list_gv_res_scroll_area->setWidgetResizable(true);
+  list_gv_res_scroll_area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+  list_gv_res_scroll_area->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
   main_lt->addWidget(list_gv);
   main_lt->addLayout(add_btn_lt);
-  main_lt->addWidget(list_gv_res);
+  main_lt->addWidget(list_gv_res_scroll_area);
 
   generate_list_gv_items();
+
+}
+
+void qgeom_view_selector_widget_t::add_btn_clicked() {
+
+  auto sel_id = list_gv->currentRow();
+  if (sel_id < 0 || sel_id >= list_gv->count()) return;
+
+  qgeom_view_selector_entry_t *new_entry = new qgeom_view_selector_entry_t;
+  new_entry->binded_gv = m_sub_gvs[sel_id];
+  new_entry->rebuild();
+  list_gv_res->main_lt->addWidget(new_entry);
+
+}
+
+qgeom_view_selector_entry_t::qgeom_view_selector_entry_t(QWidget *parent): QFrame(parent) {
+
+  main_lt = new QVBoxLayout;
+  setLayout(main_lt);
+
+  setFrameStyle(QFrame::Panel);
+  ctrls_lt = new QHBoxLayout;
+
+  gv_name = new QLabel("entry_name1");
+  gv_anim_name = new QComboBox;
+  gv_frame_id = new QComboBox;
+
+  main_lt->addWidget(gv_name);
+  ctrls_lt->addWidget(gv_anim_name);
+  ctrls_lt->addWidget(gv_frame_id);
+  ctrls_lt->addSpacing(1);
+  main_lt->addLayout(ctrls_lt);
+
+  setFixedHeight(80);
+
+}
+
+void qgeom_view_selector_entry_t::rebuild() {
+
+  if (!binded_gv) return;
+
+  std::string list_item_name = fmt::format("{}/{}",
+                                           binded_gv->m_parent_ws->m_ws_name,
+                                           binded_gv->m_name);
+
+  gv_name->setText(QString::fromStdString(list_item_name));
+
+  if (binded_gv->m_anim->animable()) {
+
+    }
+
+  gv_anim_name->setEnabled(false);
+  //gv_frame_id->
+
+}
+
+qgeom_view_result_widget_t::qgeom_view_result_widget_t(QWidget *parent) : QWidget(parent) {
+
+  main_lt = new QVBoxLayout;
+  setLayout(main_lt);
 
 }
--- src/qppcad/ws_item/geom_view/geom_view_selector_widget.hpp
+++ src/qppcad/ws_item/geom_view/geom_view_selector_widget.hpp
@@ -2,33 +2,76 @@
 #define QPPCAD_GEOM_VIEW_SELECTOR_WIDGET
 
 #include <qppcad/qppcad.hpp>
+#include <qppcad/ws_item/geom_view/geom_view.hpp>
 #include <QWidget>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QListWidget>
 #include <QListWidgetItem>
 #include <QPushButton>
+#include <QComboBox>
+#include <QLabel>
+#include <QFrame>
+#include <QScrollArea>
 
 namespace qpp {
 
   namespace cad {
 
+    class qgeom_view_selector_entry_t : public QFrame {
+
+    public:
+
+      geom_view_t *binded_gv{nullptr};
+
+      QVBoxLayout *main_lt;
+      QHBoxLayout *ctrls_lt;
+      QLabel *gv_name;
+      QComboBox *gv_anim_name;
+      QComboBox *gv_frame_id;
+      QPushButton *close_button;
+
+      explicit qgeom_view_selector_entry_t(QWidget *parent = nullptr);
+
+      void rebuild();
+
+    };
+
+    class qgeom_view_result_widget_t : public QWidget {
+
+      Q_OBJECT
+
+    public:
+
+      QVBoxLayout *main_lt;
+
+      explicit qgeom_view_result_widget_t(QWidget *parent = nullptr);
+
+    };
+
     class qgeom_view_selector_widget_t : public QWidget {
 
-        Q_OBJECT
+      Q_OBJECT
 
-      public:
+    public:
 
-        QHBoxLayout *main_lt;
-        QListWidget *list_gv{nullptr};
-        QListWidget *list_gv_res{nullptr};
+      QHBoxLayout *main_lt;
+      QListWidget *list_gv{nullptr};
+      QScrollArea *list_gv_res_scroll_area;
+      qgeom_view_result_widget_t *list_gv_res{nullptr};
 
-        QVBoxLayout *add_btn_lt;
-        QPushButton *add_btn;
+      QVBoxLayout *add_btn_lt;
+      QPushButton *add_btn;
 
-        void generate_list_gv_items();
+      std::vector<geom_view_t*> m_sub_gvs;
 
-        qgeom_view_selector_widget_t(QWidget *parent = 0);
+      void generate_list_gv_items();
+
+      qgeom_view_selector_widget_t(QWidget *parent = 0);
+
+    public slots:
+
+      void add_btn_clicked();
 
     };
 
