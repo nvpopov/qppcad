daea19696d82c5dfdd7dad81594e109ecf972b0d
nvpopov <brodiaga38@gmail.com>
2019-06-27 11:24:57 +0800

    fix(geom_view): fixed camera target view(cc)

--- src/qppcad/ws_item/geom_view/geom_view.cpp
+++ src/qppcad/ws_item/geom_view/geom_view.cpp
@@ -188,8 +188,8 @@ void geom_view_t::target_view(cam_tv_e target_view_src,
 
     case cam_tv_e::tv_cc : {
         float axis_size = std::max(2.0f, m_ext_obs->aabb.max[0] - m_ext_obs->aabb.min[0]);
-        look_from = m_pos - 2.0f*vector3<float>(axis_size, 0.0, 0.0);
-        look_to = m_pos + (m_ext_obs->aabb.max - m_ext_obs->aabb.min) * 0.5;
+        look_from = m_pos - axis_size*(m_ext_obs->aabb.max - m_ext_obs->aabb.min).normalized();
+        look_to = m_pos + (m_ext_obs->aabb.max + m_ext_obs->aabb.min) * 0.5;
         look_up = {0.0 , 0.0 , 1.0};
         need_to_update_camera = true;
         break;
