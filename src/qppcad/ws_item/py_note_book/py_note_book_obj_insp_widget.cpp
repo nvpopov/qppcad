9a7558b75a1f1f41325e868c6b8b2cae8fc89bc7
nvpopov <brodiaga38@gmail.com>
2019-09-01 07:28:54 +0800

    fix(py_note_book_t): fixed oi stretch

--- src/qppcad/ws_item/py_note_book/py_note_book_obj_insp_widget.cpp
+++ src/qppcad/ws_item/py_note_book/py_note_book_obj_insp_widget.cpp
@@ -5,12 +5,14 @@ using namespace qpp::cad;
 
 py_note_book_obj_insp_widget_t::py_note_book_obj_insp_widget_t() {
 
+  tab_general->tab_inner_widget_lt->addStretch();
+
 }
 
 void py_note_book_obj_insp_widget_t::bind_to_item(ws_item_t *_binding_item) {
 
   ws_item_obj_insp_widget_t::bind_to_item(_binding_item);
-  
+
 }
 
 void py_note_book_obj_insp_widget_t::update_from_ws_item() {
