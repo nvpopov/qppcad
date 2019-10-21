2f9c00a633f65df35b0d6ef8f2cbe930537033df
nvpopov <brodiaga38@gmail.com>
2019-10-21 21:22:06 +0800

    fix(core): std types aliasing

--- src/qppcad/core/qppcad.hpp
+++ src/qppcad/core/qppcad.hpp
@@ -23,15 +23,16 @@ namespace qpp {
 
   namespace cad {
 
-    typedef std::uint8_t  uint8_t;
-    typedef std::uint16_t uint16_t;
-    typedef std::uint32_t uint32_t;
-    typedef std::uint64_t uint64_t;
+    using size_t   = std::size_t;
+    using uint8_t  = std::uint8_t;
+    using uint16_t = std::uint16_t;
+    using uint32_t = std::uint32_t;
+    using uint64_t = std::uint64_t;
 
-    typedef std::int8_t  int8_t;
-    typedef std::int16_t int16_t;
-    typedef std::int32_t int32_t;
-    typedef std::int64_t int64_t;
+    using int8_t   = std::int8_t;
+    using int16_t  = std::int16_t;
+    using int32_t  = std::int32_t;
+    using int64_t  = std::int64_t;
 
     /* alias for std::optional*/
     template<typename T>
