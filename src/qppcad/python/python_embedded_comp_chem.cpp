63f65e5992bb82d905a421094bad439e4ddc4de1
nvpopov <brodiaga38@gmail.com>
2019-12-10 23:20:04 +0300

    fix(comp_chem_tools: added default values for parameters of orca_helper_t::gen_coord_section

--- src/qppcad/python/python_embedded_comp_chem.cpp
+++ src/qppcad/python/python_embedded_comp_chem.cpp
@@ -15,8 +15,10 @@ PYBIND11_EMBEDDED_MODULE(cc, m) {
   auto module_orca = m.def_submodule("orca");
   module_orca.def("gen_multijob_from_anim", &orca_helper_t::gen_multijob_from_anim);
   module_orca.def("gen_coord_section", &orca_helper_t::gen_coord_section,
-                  py::arg("gv"), py::arg("is_point_charge"),
-                  py::arg("basis_less_cnt"), py::arg("ecp_name"));
+                  py::arg("gv"),
+                  py::arg("is_point_charge") = std::vector<std::string>{},
+                  py::arg("basis_less_cnt") = std::vector<std::string>{},
+                  py::arg("ecp_name") = std::map<std::string, std::string>{});
 
   m.def("get_raw_coords_section", &generic_qc_helper_t::get_raw_coords_section);
 
