d41f802c801f15680dd9fe5ee1cb0cf8beaba7d4
nvpopov <brodiaga38@gmail.com>
2019-12-31 15:04:51 +0300

    fix(register_all)things): added tool_type to tool's registration

--- src/qppcad/core/register_all_things.hpp
+++ src/qppcad/core/register_all_things.hpp
@@ -12,34 +12,42 @@ namespace qpp {
 
         template<typename T>
         static void reg_ws_item_fbr(ws_item_behaviour_manager_t *bhv_mgr) {
+
           bhv_mgr->reg_item_fbr(T::get_type_static(),
                                 [](){return std::make_shared<T>();});
+
         }
 
         template<typename T, typename U>
         static void reg_ws_item_obj_insp_fbr(ws_item_behaviour_manager_t *bhv_mgr) {
+
           bhv_mgr->reg_obj_insp_fbr(T::get_type_static(),
                                     [](){return std::make_shared<U>();});
+
         }
 
         template<typename T, typename U>
         static void reg_ws_item_ext_edt_fbr(size_t editor_order,
                                             std::string editor_name,
                                             ws_item_behaviour_manager_t *bhv_mgr) {
+
           bhv_mgr->reg_ext_editor_fbr(T::get_type_static(),
                                       editor_order,
                                       editor_name,
                                       [](){return std::make_shared<U>();});
+
         }
 
         template<typename T>
         static void reg_toolbar_elements_fbr(ws_item_behaviour_manager_t *bhv_mgr,
                                              std::size_t tb_el_hash,
                                              const std::string &tb_el_name) {
+
           bhv_mgr->reg_toolbar_elem_fbr(T::get_type_static(),
                                         tb_el_hash,
                                         tb_el_name,
                                         [](){return std::make_shared<T>();});
+
         }
 
         static void reg_ws_item_fbr(ws_item_behaviour_manager_t *bhv_mgr);
@@ -52,12 +60,16 @@ namespace qpp {
         static void reg_ws_item_tool(std::string _full_name,
                                      size_t _g_hash,
                                      ws_item_behaviour_manager_t *bhv_mgr,
-                                     bool _item_required = true) {
+                                     bool _item_required = true,
+                                     ws_item_tool_type_e tool_type =
+                                      ws_item_tool_type_e::ws_item_tool_modal) {
+
           bhv_mgr->reg_tool(_full_name,
                             _g_hash,
                             U::get_type_static(),
                             _item_required,
                             [](){return std::make_shared<T>();});
+
         }
 
         static void reg_ws_item_tools(ws_item_behaviour_manager_t *bhv_mgr);
