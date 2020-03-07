37665c2682aaa16ef857c6a757e0342ddc3e7e72
nvpopov <brodiaga38@gmail.com>
2020-03-08 00:08:14 +0300

    fix(history_stream): added posibility to traverse history line

--- src/qppcad/core/history_stream.hpp
+++ src/qppcad/core/history_stream.hpp
@@ -19,7 +19,17 @@ namespace qpp {
 
     public:
 
-      void advance_epoch(STORED_TYPE &&new_value, std::optional<size_t> new_epoch = std::nullopt) {
+      void push_epoch(STORED_TYPE &&new_value, std::optional<size_t> new_epoch = std::nullopt) {
+
+        // do we insert at end of current history line?
+        // if not - truncate history line
+        if (p_cur_history_epoch != p_max_history_epoch) {
+            auto it_cur = std::find(std::begin(p_history_line),
+                                    std::end(p_history_line),
+                                    p_cur_history_epoch);
+            if (it_cur != p_history_line.end())
+              p_history_line.erase(++it_cur, std::end(p_history_line));
+          }
 
         // provide epoch index or do incremental epoch
         size_t new_epoch_ex =
@@ -31,10 +41,52 @@ namespace qpp {
 
       }
 
+      bool epoch_exists_in_current_history(size_t epoch_id) {
+        auto it1 = std::find(std::begin(p_history_line), std::end(p_history_line), epoch_id);
+        return it1 != p_history_line.end();
+      }
+
       size_t get_cur_epoch() {
-
         return p_cur_history_epoch;
+      }
 
+      void set_cur_epoch(size_t epoch_id) {
+        auto it1 = std::find(std::begin(p_history_line), std::end(p_history_line), epoch_id);
+        if (it1 != p_history_line.end()) p_cur_history_epoch = epoch_id;
+      }
+
+      std::optional<STORED_TYPE> get_epoch_value(size_t epoch_id) {
+        auto it1 = p_value_stream.find(epoch_id);
+        return it1 != p_value_stream.end() ?
+               std::optional<STORED_TYPE>(it1.second) : std::nullopt;
+      }
+
+      size_t get_cur_history_line_size() {
+        return p_history_line.size();
+      }
+
+      void print_debug() {
+
+        std::cout << std::endl << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << std::endl;
+
+        for (size_t i = 0; i < p_history_line.size(); i++)
+          std::cout << "Epoch id = "
+                    << p_history_line[i]
+                    << ", value = "
+                    << p_value_stream[p_history_line[i]]
+                    << std::endl;
+
+
+        std::cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << std::endl;
+
+      }
+
+      history_stream_t() {
+
+      }
+
+      history_stream_t(STORED_TYPE&& default_value) {
+        p_value_stream[0] = default_value;
       }
 
     };
--- src/qppcad/core/qppcad_tests.cpp
+++ src/qppcad/core/qppcad_tests.cpp
@@ -11,33 +11,47 @@ TEST_CASE("history stream test") {
   SECTION ("basic features") {
 
     history_stream_t<int> hints;
-    hints.advance_epoch(2);
-    hints.advance_epoch(3);
-    hints.advance_epoch(5);
+    hints.push_epoch(2);
+    hints.push_epoch(3);
+    hints.push_epoch(5);
 
     REQUIRE(hints.get_cur_epoch() == 3);
+    REQUIRE(hints.epoch_exists_in_current_history(0));
+    REQUIRE(hints.epoch_exists_in_current_history(1));
+    REQUIRE(hints.epoch_exists_in_current_history(2));
+    REQUIRE(hints.epoch_exists_in_current_history(3));
+    REQUIRE(!hints.epoch_exists_in_current_history(4));
+    hints.print_debug();
+
+    hints.set_cur_epoch(2);
+    hints.push_epoch(34);
+    hints.print_debug();
+    REQUIRE(hints.get_cur_epoch() == 4);
+    REQUIRE(hints.get_cur_history_line_size() == 4);
 
   }
 
   SECTION ("basic features + gen epoch") {
 
     history_stream_t<int> hints;
-    hints.advance_epoch(2);
-    hints.advance_epoch(3);
-    hints.advance_epoch(5);
-    hints.advance_epoch(6, 10);
+    hints.push_epoch(2);
+    hints.push_epoch(3);
+    hints.push_epoch(5);
+    hints.push_epoch(6, 10);
 
     REQUIRE(hints.get_cur_epoch() == 10);
+    REQUIRE(!hints.epoch_exists_in_current_history(5));
+    REQUIRE(hints.epoch_exists_in_current_history(10));
 
   }
 
   SECTION ("basic features + gen epoch") {
 
     history_stream_t<int> hints;
-    hints.advance_epoch(2);
-    hints.advance_epoch(3);
-    hints.advance_epoch(5);
-    hints.advance_epoch(6, 2);
+    hints.push_epoch(2);
+    hints.push_epoch(3);
+    hints.push_epoch(5);
+    hints.push_epoch(6, 2);
 
     REQUIRE(hints.get_cur_epoch() == 4);
 
