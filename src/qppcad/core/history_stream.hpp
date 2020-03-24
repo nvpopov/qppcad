fc974a26e322e935f554d56cb6a16ff446b91c92
nvpopov <brodiaga38@gmail.com>
2020-03-24 23:25:29 +0300

    fix(history_stream): basic children handling

--- src/qppcad/core/history_stream.hpp
+++ src/qppcad/core/history_stream.hpp
@@ -15,6 +15,12 @@ namespace qpp {
       delta_incremental
     };
 
+    enum history_result_e {
+      hr_error = 0,
+      hr_success = 1,
+      hr_invalid_epoch = 2,
+    };
+
     class history_document_base_t {
 
     public:
@@ -29,36 +35,37 @@ namespace qpp {
       std::map<epoch_t, std::vector<std::tuple<self_t*, epoch_t>>> p_childs_states;
       std::set<epoch_t> p_history_line{0};
       bool p_is_bad{false};
+      bool p_is_dirty{false};
 
     public:
 
       epoch_t get_cur_epoch() { return p_cur_epoch; }
-      bool set_cur_epoch(epoch_t cur_epoch) {
+      history_result_e set_cur_epoch(epoch_t cur_epoch) {
 
         auto it_ce = std::find(std::begin(p_history_line), std::end(p_history_line), cur_epoch);
         if (it_ce != std::end(p_history_line)) {
             p_cur_epoch = cur_epoch;
-            return true;
+            return history_result_e::hr_success;
           }
 
-        return false;
+        return history_result_e::hr_invalid_epoch;
 
       }
 
-      bool push_epoch(epoch_t new_epoch) {
+      history_result_e push_epoch(epoch_t new_epoch) {
 
         if (p_history_line.empty()) {
             p_history_line.insert(new_epoch);
-            return true;
+            return history_result_e::hr_success;
           }
 
         auto last = p_history_line.end();
-        if (new_epoch <= *last) {
+        if (new_epoch > *last) {
             p_is_bad = true;
-            return false;
+            return history_result_e::hr_invalid_epoch;
           } else {
             p_history_line.insert(new_epoch);
-            return true;
+            return history_result_e::hr_success;
           }
 
       }
@@ -76,6 +83,37 @@ namespace qpp {
       void checkout_to_epoch(epoch_t target_epoch) {
 
       }
+
+      //children stuff
+      void add_child(self_t *child) {
+        if (child) p_childs.push_back(child);
+      }
+
+      self_t *get_children(size_t idx) {
+        if (idx < p_childs.size())
+          return p_childs[idx];
+        return nullptr;
+      }
+
+      std::optional<size_t> is_children(self_t *child) {
+        if (!child) return std::nullopt;
+        auto it1 = std::find(std::begin(p_childs), std::end(p_childs), child);
+        return (it1 != std::end(p_childs)
+                ? std::optional<size_t>{std::distance(std::begin(p_childs), it1)} : std::nullopt);
+      }
+
+      void remove_child(size_t child_id) {
+        if (child_id < p_childs.size())
+          p_childs.erase(std::begin(p_childs) + child_id);
+      }
+
+      void remove_child(self_t *child) {
+        auto it1 = std::find(std::begin(p_childs), std::end(p_childs), child);
+        if (it1 != std::end(p_childs)) p_childs.erase(it1);
+      }
+
+      auto get_children_count() { return p_childs.size(); }
+      //end of children stuff
 
     };
 
--- src/qppcad/core/qppcad_tests.cpp
+++ src/qppcad/core/qppcad_tests.cpp
@@ -12,12 +12,30 @@ TEST_CASE("history stream test") {
 
     history_document_base_t *hs = new history_document_base_t;
     REQUIRE(hs->get_cur_epoch() == 0);
-    REQUIRE(hs->set_cur_epoch(2) == false);
+    REQUIRE(hs->set_cur_epoch(2) == history_result_e::hr_invalid_epoch);
 
-    REQUIRE(hs->push_epoch(1));
-    REQUIRE(hs->push_epoch(2));
+    REQUIRE(hs->push_epoch(1) == history_result_e::hr_success);
+    REQUIRE(hs->push_epoch(2) == history_result_e::hr_success);
 
     REQUIRE(hs->get_history_size() == 3);
+
+    history_document_base_t *hs_c1 = new history_document_base_t;
+    history_document_base_t *hs_c2 = new history_document_base_t;
+    history_document_base_t *hs_c3 = new history_document_base_t;
+    history_document_base_t *hs_nc = new history_document_base_t;
+
+    hs->add_child(hs_c1);
+    hs->add_child(hs_c2);
+
+    REQUIRE(hs->get_children_count() == 2);
+    REQUIRE(*(hs->is_children(hs_c1)) == 0);
+    REQUIRE(*(hs->is_children(hs_c2)) == 1);
+    REQUIRE(hs->is_children(hs_nc) == std::nullopt);
+
+    hs->add_child(hs_c3);
+    REQUIRE(hs->get_children_count() == 3);
+    hs->remove_child(hs_c3);
+    REQUIRE(hs->get_children_count() == 2);
 
   }
 
