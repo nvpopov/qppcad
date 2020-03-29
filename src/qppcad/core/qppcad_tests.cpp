61eda1d448fa7217a0bdf6658b1709fb36ac3b16
nvpopov <brodiaga38@gmail.com>
2020-03-29 23:19:18 +0300

    fix(history_need): added test for nested elements

--- src/qppcad/core/qppcad_tests.cpp
+++ src/qppcad/core/qppcad_tests.cpp
@@ -64,4 +64,55 @@ TEST_CASE("history stream test") {
 
   }
 
+  SECTION("Deep nested structure") {
+
+    hist_doc_base_t *hs = new hist_doc_base_t;
+    hist_doc_base_t *hs_c1 = new hist_doc_base_t;
+    hist_doc_base_t *hs_c1_c1 = new hist_doc_base_t;
+    hist_doc_base_t *hs_c1_c2 = new hist_doc_base_t;
+    hist_doc_base_t *hs_c2 = new hist_doc_base_t;
+    hist_doc_base_t *hs_c3 = new hist_doc_base_t;
+
+    hs->add_child(hs_c1);
+    hs->add_child(hs_c2);
+    hs->add_child(hs_c3);
+
+    hs_c1->add_child(hs_c1_c1);
+    hs_c1->add_child(hs_c1_c2);
+
+    hs->push_epoch(1);
+    hs_c1->push_epoch(1);
+    hs_c1_c1->push_epoch(1);
+    hs_c1_c2->push_epoch(1);
+    hs_c2->push_epoch(1);
+    hs_c3->push_epoch(1);
+
+    hs->augment_epoch(hs_c1, 0, 0);
+    hs->augment_epoch(hs_c2, 0, 0);
+    hs->augment_epoch(hs_c3, 0, 0);
+    hs->augment_epoch(hs_c1, 1, 1);
+    hs->augment_epoch(hs_c2, 1, 1);
+    hs->augment_epoch(hs_c3, 1, 1);
+
+    hs_c1->augment_epoch(hs_c1_c1, 0, 0);
+    hs_c1->augment_epoch(hs_c1_c2, 0, 0);
+    hs_c1->augment_epoch(hs_c1_c1, 1, 1);
+    hs_c1->augment_epoch(hs_c1_c2, 1, 1);
+
+    REQUIRE(hs->checkout_to_epoch(1) == hr_result_e::hr_success);
+    REQUIRE(hs_c1->get_cur_epoch() == 1);
+    REQUIRE(hs_c2->get_cur_epoch() == 1);
+    REQUIRE(hs_c3->get_cur_epoch() == 1);
+    REQUIRE(hs_c1_c1->get_cur_epoch() == 1);
+    REQUIRE(hs_c1_c2->get_cur_epoch() == 1);
+
+    REQUIRE(hs->checkout_to_epoch(0) == hr_result_e::hr_success);
+    REQUIRE(hs_c1->get_cur_epoch() == 0);
+    REQUIRE(hs_c2->get_cur_epoch() == 0);
+    REQUIRE(hs_c3->get_cur_epoch() == 0);
+    REQUIRE(hs_c1_c1->get_cur_epoch() == 0);
+    REQUIRE(hs_c1_c2->get_cur_epoch() == 0);
+
+  }
+
 }
