e8ed8f6e1eee2ec3df22128cd54d587ab6ccd31b
nvpopov <brodiaga38@gmail.com>
2020-08-05 16:34:46 +0800

    fix(python_mgr): fixing redundant newline symbol while output the string

--- src/qppcad/python/python_manager.cpp
+++ src/qppcad/python/python_manager.cpp
@@ -26,16 +26,24 @@ bool python_manager_t::execute(std::string command) {
         if (!comm_has_equality && !comm_is_multiline && !comm_has_statement && !comm_is_multi) {
             py::object res = py::eval(command, py::globals());
 
-            if (py::isinstance<py::int_>(res) || py::isinstance<py::float_>(res) ||
-                py::isinstance<py::str>(res) || py::isinstance<py::list>(res) ||
-                py::isinstance<py::dict>(res) || py::isinstance<py::bool_>(res) ||
-                py::isinstance<vector3<float> >(res) || py::isinstance<matrix3<float> >(res)) {
+            if (py::isinstance<py::int_>(res)
+                || py::isinstance<py::float_>(res)
+                || py::isinstance<py::list>(res)
+                || py::isinstance<py::dict>(res)
+                || py::isinstance<py::bool_>(res)
+                || py::isinstance<vector3<float> >(res)
+                || py::isinstance<matrix3<float> >(res)) {
                 py::print(res);
-              }
-          } else {
+            } else if (py::isinstance<py::str>(res)) {
+              std::string as_str = py::cast<py::str>(res);
+              if (as_str[as_str.size()-1] == '\n') as_str.resize(as_str.size()-1);
+              py::print(as_str);
+            } else {
             py::eval<py::eval_statements>(command, py::globals());
           }
       }
+
+    }
 
   } catch (py::error_already_set &err) {
 
